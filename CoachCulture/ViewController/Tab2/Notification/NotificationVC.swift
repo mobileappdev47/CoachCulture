ab86d5dc0f469aa186389a8435fa34dc868d78dc
Om <61979540+mobiledev48@users.noreply.github.com>
2022-03-12 18:12:09 +0530

    3.notification - continue

---
+++ CoachCulture/ViewController/Tab2/Notification/NotificationVC.swift
@@ -0,0 +1,21 @@
+
+import UIKit
+
+class NotificationVC: UIViewController {
+
+    //MARK: - OUTLET
+    
+    //MARK: - VARIABLE AND OBJECT
+    
+    //MARK: - VIEW CONTROLLER LIFE CYCLE
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    //MARK: - FUNCTION
+    
+}
+
+//MARK: - EXTENSION TABLE DATASOURCE AND DELEGATE
